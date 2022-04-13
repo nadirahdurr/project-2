17fb5ad70f8a120f40ca37af8e599e15f71e1e0b
Nadirah Durr <31542146+nadirahdurr@users.noreply.github.com>
2022-04-13 18:35:14 -0400

    fixed directory

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+    <Route path="/" element={<Login />} />
+    <Route path="dashboard" element={<Dashboard />} />
+  </Routes>
+
+  );
+}
+
+export default App;
